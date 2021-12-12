bb114eceabc2f16ae4a092b7f05fd8adb2bffa22
adnan <alhossain.a92@gmail.com>
2021-12-12 00:39:13 -0800

    student controller and model created

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-   <!doctype html>
+          <!doctype html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
@@ -16,11 +11,62 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>Hello, world!</title>
+
+
+
   </head>
   <body>
-    <h1>Hello, world!</h1>
+   <div class="card  mb-3">
+  <div class="card-header text-center">Student</div>  
+<div class="row">
+  <div class="col-sm-10 m-auto">
+    <a class="btn btn-success btn-sm" href="{{route('student.create')}}" title="">Add new Student+</a>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Age</th>
+      <th scope="col">Roll</th>
+      <th scope="col">Phone</th>
+      <th scope="col">Action</th>
 
-    <!-- Optional JavaScript; choose one of the two! -->
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+
+    </tr>
+   
+  </tbody>
+</table>
+   
+  </div>
+ 
+</div>
+   
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -32,5 +78,8 @@
     -->
   </body>
 </html>
+    </x-slot>
+
+
 
 </x-app-layout>
